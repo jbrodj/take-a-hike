0fc0d5cad3636718dca2c26f3610e53fee395e96
J Brodie Day <jbrodieday@gmail.com>
2024-11-25 16:31:24 -0500

    refactor: cleanup and rm double quotes

--- app.py
+++ app.py
@@ -2,29 +2,25 @@ from flask import Flask, redirect, render_template, request
 from content import formContent
 
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.after_request
 def after_request(response):
   # Source: CS50
-  """Ensure responses aren't cached"""
-  response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-  response.headers["Expires"] = 0
-  response.headers["Pragma"] = "no-cache"
+  '''Ensure responses aren't cached'''
+  response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  response.headers['Expires'] = 0
+  response.headers['Pragma'] = 'no-cache'
   return response
 
 @app.route('/')
 def hello():
-  print(formContent.keys())
-  for field in formContent:
-    print(formContent[field]["name"])
-  return render_template("index.html")
+  return render_template('index.html')
 
-@app.route('/new-hike')
+@app.route('/new-hike', methods=['GET', 'POST'])
 def newHike():
-  if request.method == "POST":
-    print('post')
+  if request.method == 'POST':
     return redirect('/')
   else:
-    return render_template("new-hike.html", formContent=formContent)
+    return render_template('new-hike.html', formContent=formContent)
