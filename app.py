1d67e0ea39a115967fae2f5a69abdd6d2cda2b51
J Brodie Day <jbrodieday@gmail.com>
2024-11-27 12:02:54 -0500

    refactor(app): lint and add docstrings

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''For rendering, routing, and accessing request properties'''
 from flask import Flask, redirect, render_template, request
 from content import formContent
 
@@ -7,20 +8,22 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.after_request
 def after_request(response):
-  # Source: CS50
-  '''Ensure responses aren't cached'''
-  response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-  response.headers['Expires'] = 0
-  response.headers['Pragma'] = 'no-cache'
-  return response
+    '''Ensure responses aren't cached. Source: CS50'''
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Expires'] = 0
+    response.headers['Pragma'] = 'no-cache'
+    return response
 
 @app.route('/')
-def hello():
-  return render_template('index.html')
+def index():
+    '''Renders default template at base route.'''
+    return render_template('index.html')
 
 @app.route('/new-hike', methods=['GET', 'POST'])
-def newHike():
-  if request.method == 'POST':
-    return redirect('/')
-  else:
+def new_hike():
+    '''Renders new hike form template on GET, or submits hike data to db on POST.'''
+    if request.method == 'POST':
+        hike_data = request.form
+        print(hike_data)
+        return redirect('/')
     return render_template('new-hike.html', formContent=formContent)
