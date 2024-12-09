daec35b17fb38f0c1aaaa07b5898ab62da889eed
J Brodie Day <jbrodieday@gmail.com>
2024-12-09 15:22:24 -0500

    feat(error-handling): update calls to hande_error with request.url arg

--- app.py
+++ app.py
@@ -30,23 +30,27 @@ def sign_up():
     '''Renders sign-up form template on GET, or submits new user to db on POST'''
     if request.method == 'POST':
         username = request.form.get('username')
-        # Default method for generate_password_hash (scrypt) doesn't work on macOS, so switching to 'pbdkf2' for development
+        # Default method for generate_password_hash (scrypt) doesn't work on macOS, 
+        # so switching to 'pbdkf2' for development
         password_hash = generate_password_hash(request.form.get('password'), method='pbkdf2')
-        # confirmation_hash = generate_password_hash(request.form.get('confirmation'), method='pbkdf2')
         existing_usernames = utils.get_all_usernames(DB)
         print(f'existing_usernames: {existing_usernames}')
         if not username or not request.form.get('password') or not request.form.get('confirmation'):
-            return utils.handle_error('Username and password are required', 403)
+            return utils.handle_error(request.url, 'Username and password are required', 403)
+        if not username.isalnum():
+            return utils.handle_error(
+                request.url, 'Username must contain only letters and numbers', 403)
         if not request.form.get('password') == request.form.get('confirmation'):
-            return utils.handle_error('Passwords must match', 403)
+            return utils.handle_error(request.url, 'Passwords must match', 403)
         if username:
             for existing_name in existing_usernames:
-                if username == existing_name['username']:
-                    return utils.handle_error('Username is already taken', 403)
+                if username == existing_name[0]:
+                    return utils.handle_error(request.url, 'Username is already taken', 403)
         utils.add_user(username, password_hash)
         return redirect('/login')
 
     return render_template('signup.html')
+
 
 @app.route('/new-hike', methods=['GET', 'POST'])
 def new_hike():
@@ -54,8 +58,9 @@ def new_hike():
     if request.method == 'POST':
         form_data = request.form
         for value in form_data:
-            if form_data.get(value) == '' and form_content[value]['required'] is True:
-                return utils.handle_error(403, 'Date, area, trailhead, and trails are required values.')
+            if form_data.get(value) == '' and new_hike_form_content[value]['required'] is True:
+                return utils.handle_error(
+                    request.url, 'Date, area, trailhead, and trails are required values.', 403)
 
         hike_data = utils.format_hike_form_data(form_data)
         area_name = hike_data['area_name']
