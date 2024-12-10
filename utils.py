2e960e3bc5879a96b4db1443817f8f21b1296075
J Brodie Day <jbrodieday@gmail.com>
2024-12-10 17:02:38 -0500

    refactor(utils): update arg order in add_hike and add user id to insert command

--- utils.py
+++ utils.py
@@ -75,15 +75,15 @@ def add_trail(area_id, trail_names):
     commit_close_conn(db_connection['connection'])
 
 
-def add_hike(hike_data, area_id):
+def add_hike(user_id, area_id, hike_data):
     '''Takes hike data from form and area id from database.
         Creates new hike in hikes table and inserts data.
     '''
     hike_date, area_name, trailhead, trails_cs, distance_km, image_url, image_alt, map_link, other_info = hike_data.values()
     db_connection = create_connection('hikes.db')
     db_connection['cursor'].execute(
-        'INSERT INTO hikes (hike_date, area_id, area_name, trailhead, trails_cs, distance_km, image_url, image_alt, map_link, other_info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            [hike_date, area_id, area_name, trailhead, trails_cs, distance_km, image_url, image_alt, map_link, other_info])
+        'INSERT INTO hikes (hike_date, user_id, area_id, area_name, trailhead, trails_cs, distance_km, image_url, image_alt, map_link, other_info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [hike_date, user_id, area_id, area_name, trailhead, trails_cs, distance_km, image_url, image_alt, map_link, other_info])
     commit_close_conn(db_connection['connection'])
 
 # RETRIEVE DATA FROM DATABASE
