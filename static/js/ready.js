130cb61cd2c2a531d72621278b4a594ce55578c5
J Brodie Day <jbrodieday@gmail.com>
2024-12-25 13:56:57 -0500

    feat(js): create function to close context message block

--- static/js/ready.js
+++ static/js/ready.js
@@ -1,6 +1,6 @@
 // Prevent layout shift from content loading before styles are loaded.
 
-let ready = (callback) => {
+const ready = (callback) => {
   document.readyState === 'complete' || document.readyState === 'interactive'
     ? callback()
     :document.addEventListener('DOMContentLoaded', callback)
@@ -9,3 +9,20 @@ let ready = (callback) => {
 ready(() => {
   document.body.style.visibility = 'visible'
 })
+
+ready()
+
+
+// Provide functionality for close button on context message container
+
+const closeContextMsg = () => {
+  msgContainer = document.getElementById('contextMsgContainer')
+  closeBtn = document.getElementById('contextCloseBtn')
+
+  closeBtn?.addEventListener('click', (event) => {
+    console.log('close btn event')
+    msgContainer.classList.add('hidden')
+  })
+}
+
+closeContextMsg()
