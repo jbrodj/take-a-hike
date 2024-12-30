453315d7e4e4592f05f8dba21d30b7ba259bc2bb
J Brodie Day <jbrodieday@gmail.com>
2024-12-30 11:41:30 -0500

    feat(content): update structure of hike_form_content to update form for img file upload

--- content.py
+++ content.py
@@ -33,27 +33,27 @@ hike_form_content = {
         'inputType': 'number',
         'required': True
     },
-    'image_url': {
+        'image_url': {
         'name': 'image_url',
-        'label': 'Image URL',
-        'inputType': 'text',
+        'label': 'Image',
+        'inputType': 'file',
         'required': False
     },
     'image_alt': {
         'name': 'image_alt',
-        'label': 'Image description',
-        'inputType': 'text',
-        'required': False
-    },
-    'map_link': {
-        'name': 'map_link',
-        'label': 'Map link',
+        'label': 'Image alt text',
         'inputType': 'text',
         'required': False
     },
     'other_info': {
         'name': 'other_info',
         'label': 'Description',
+        'inputType': 'text',
+        'required': False
+    },
+    'map_link': {
+        'name': 'map_link',
+        'label': 'Map link',
         'inputType': 'text',
         'required': False
     }
