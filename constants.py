fc389f42dd4784a2385580ffb643db536be02886
J Brodie Day <jbrodieday@gmail.com>
2024-12-30 14:48:53 -0500

    feat(app): add cloudinary urls to constants

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 '''Constants for python functions'''
+# pylint: disable=line-too-long
 # Database file string
 DB = 'hikes.db'
+# Cloudinary urls for limit fill specified width, and quality
+CLOUDINARY_URL_900 = 'https://res.cloudinary.com/take-a-hike/image/upload/c_lfill,w_900/q_auto:best/'
+CLOUDINARY_URL_100 = 'https://res.cloudinary.com/take-a-hike/image/upload/c_lfill,w_100/q_auto:good/'
